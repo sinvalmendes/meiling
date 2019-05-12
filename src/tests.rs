b0f09c629a0b776eea4b0e90d302554359303b98
Sinval Vieira <sinvalneto01@gmail.com>
2019-05-12 22:12:32 +0200

    added test scenario to open file with vim

--- src/tests.rs
+++ src/tests.rs
@@ -3,6 +3,7 @@ mod tests {
     use crate::main;
     use crate::get_settings;
     use crate::get_repository_url;
+    use crate::open_note_editor;
 
     #[test]
     fn test_get_settings() {
@@ -20,6 +21,11 @@ mod tests {
         let url = get_repository_url();
         println!("{:?}", url);
         assert_eq!("https://github.com/sinvalmendes/notes", url);
+    }
+
+    #[test]
+    fn test_open_note_editor() {
+        open_note_editor("file");
     }
 
 }
